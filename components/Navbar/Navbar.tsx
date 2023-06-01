27d156199041af75fad7aa82ab45e7996ae5edb6
Jan Virgen <janxavierv17@hotmail.com>
2023-06-02 02:41:14 +1000

    feat: use styled-components over tailwindcss

---
+++ components/Navbar/Navbar.tsx
@@ -0,0 +1,17 @@
+"use client";
+import Link from "next/link";
+
+export default function Navbar() {
+  return (
+    <nav>
+      <ul className="flex gap-4">
+        <li>
+          <Link href="/">Home</Link>
+        </li>
+        <li>
+          <Link href="/products">Products</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
