0f28d1d4840276dbb4d61c6233e4d26c20237ca5
Jan Virgen <janxavierv17@hotmail.com>
2023-06-02 02:32:41 +1000

    feat: initial mock service handler

--- jest.setup.ts
+++ jest.setup.ts
@@ -3,4 +3,11 @@
 
 // Used for __tests__/testing-library.js
 // Learn more: https://github.com/testing-library/jest-dom
+// Polyfill "window.fetch" used in the React component.
+import "whatwg-fetch";
 import "@testing-library/jest-dom/extend-expect";
+import { server } from "./mocks/server";
+
+beforeAll(() => server.listen()); // Start handling requests at the beginning of the tests
+afterEach(() => server.resetHandlers()); // Reset any runtime handlers after each test
+afterAll(() => server.close()); // Stop handling requests after the tests are done
---
+++ mocks/handlers.ts
@@ -0,0 +1,25 @@
+import { rest } from "msw";
+export const handlers = [
+	rest.get("https://fakestoreapi.com/products", (req, res, ctx) => {
+		console.log("Request =>", req);
+		console.log("Response =>", res);
+		console.log("CTX =>", ctx);
+		const data = [
+			{
+				id: 1,
+				title: "BLAH Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
+				price: 109.95,
+				description:
+					"Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+				category: "men's clothing",
+				image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+				rating: {
+					rate: 3.9,
+					count: 120,
+				},
+			},
+		];
+
+		return res(ctx.json(data));
+	}),
+];
