a478f7cace55ec92897e151788f4a6f8e17920f4
Jan Virgen <janxavierv17@hotmail.com>
2023-06-02 02:33:20 +1000

    test: initial tests for the project

---
+++ test/productsLayout.test.tsx
@@ -0,0 +1,11 @@
+import { render, screen } from "@testing-library/react";
+import ProductsLayout from "@/app/products/layout";
+
+describe("productsList component", () => {
+	test("Should fetch products from api", async () => {
+		render(await ProductsLayout());
+		screen.debug();
+		const image = screen.getByRole("img");
+		expect(image).oBeInTheDocument();
+	});
+});
--- test/rootPage.test.tsx
+++ test/rootPage.test.tsx
@@ -4,7 +4,6 @@ import Home from "../app/page";
 describe("Home", () => {
 	test("Home page", () => {
 		render(<Home />);
-		screen.debug();
 
 		const header = screen.getByRole("heading", { name: /home page/i });
 		expect(header).toBeInTheDocument();
